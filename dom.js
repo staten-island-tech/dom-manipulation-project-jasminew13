d91d6ba934b30a3b1750cfb1f729259fa16732d1
jasminew13 <jasminewu101@gmail.com>
2023-10-20 10:00:06 -0400

    oh

--- dom.js
+++ dom.js
@@ -1,23 +1,22 @@
 const DOMselectors = {
-    form: document.getElementById("#form"),
+    form: document.getElementById("form"),
     card: document.getElementById("card"),
-    points: document.querySelectorAll(".point"),
 };
 console.log(DOMselectors);
 
-function containerbox(box, text) {
-    box.style.boxColor = "white";
+function cardAndtext(card, text) {
+    card.style.boxColor = "red";
     text.innerHTML = "box";
-    text.textContent = "IEUHHFAEWPOIMHGCHO";
+    text.textContent = "ok";
     text.style.fontSize = "40px";
 }
 containerbox(DOMselectors.box, DOMselectors.text);
 
 DOMselectors.form.addEventListener("submit", function(event) {
-    let input = DOMselectors.form.value;
+    this.querySelector(".gallery");
     event.preventDefault();
-    DOMselectors.card.insertAdjacentHTML("afterend", 
-    `<div class="card"><h2 class="cardheader">${DOMselectors.form.value}</h2></div></div>`);
+    DOMselectors.card.insertAdjacentHTML("beforeend", 
+    `<div class="card"><h2 class="cardheader">${DOMselectors.form.value}</h2></div>`);
 });
 DOMselectors.button.addEventListener("click", function (){
     box(DOMselectors.box, DOMselectors.text);

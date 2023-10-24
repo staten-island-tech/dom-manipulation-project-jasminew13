ab90519b96db6f558f3048510ccdb1b6d54eb892
jasminew13 <jasminewu101@gmail.com>
2023-10-24 10:13:42 -0400

    kqlu34htv

--- dom.js
+++ dom.js
@@ -1,17 +1,21 @@
 const DOMselectors = {
     form: document.getElementById("form"),
+    artistname: document.getElementById("name"),
+    img: document.getElementById("image"),
     card: document.getElementById("card"),
 };
 console.log(DOMselectors);
 
-function cardAndtext(card, text) {
+/*function cardAndtext(card, text) {
     card.style.boxColor = "red";
     text.innerHTML = "box";
     text.textContent = "ok";
     text.style.fontSize = "40px";
 }
 containerbox(DOMselectors.box, DOMselectors.text);
+*/
 
+    
 DOMselectors.form.addEventListener("submit", function(event) {
     this.querySelector(".gallery");
     event.preventDefault();
@@ -21,3 +25,8 @@ DOMselectors.form.addEventListener("submit", function(event) {
 DOMselectors.button.addEventListener("click", function (){
     box(DOMselectors.box, DOMselectors.text);
 }); 
+
+//const album = make album
+//addcard(album)
+//clear fields
+//addremove buttons

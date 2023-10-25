3c73158dc2489289365a8aed5446ad9aab5e2b6c
jasminew13 <jasminewu101@gmail.com>
2023-10-25 10:15:37 -0400

    k

--- dom.js
+++ dom.js
@@ -1,28 +1,31 @@
 const DOMselectors = {
-    form: document.getElementById("form"),
-    artistname: document.getElementById("name"),
+ /*   artistname: document.getElementById("name"),
     img: document.getElementById("image"),
-    card: document.getElementById("card"),
+    card: document.getElementById("card"),*/
 };
 console.log(DOMselectors);
 
-/*function cardAndtext(card, text) {
-    card.style.boxColor = "red";
-    text.innerHTML = "box";
-    text.textContent = "ok";
-    text.style.fontSize = "40px";
-}
-containerbox(DOMselectors.box, DOMselectors.text);
-*/
+// let me cook pls
+list.addEventListener('click', function(ok){
+     if (ok.target.className == 'delete'){
+        const li = ok.target.parentElement;
+        list.removeChild(li);
+     }
+});
+
+const addForm = document.forms['add-album'];
+
+addForm.addEventListener('submit', function(ok){
+    ok.preventDefault();
+    document.querySelector('input[type="text"]').value;
+    addForm.card.insertAdjacentHTML("afterend", 
+    `<div class="card"><h2 class="cardheader">${DOMselectors.form.value}</h2></div>`);
+}); 
+
+    albumName.textContent = value;
 
     
-DOMselectors.form.addEventListener("submit", function(event) {
-    this.querySelector(".gallery");
-    event.preventDefault();
-    DOMselectors.card.insertAdjacentHTML("beforeend", 
-    `<div class="card"><h2 class="cardheader">${DOMselectors.form.value}</h2></div>`);
-});
-DOMselectors.button.addEventListener("click", function (){
+/*DOMselectors.button.addEventListener("click", function (){
     box(DOMselectors.box, DOMselectors.text);
 }); 
 

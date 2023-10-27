bd51ad112d106a85e47839f057d5208a2eeb6873
jasminew13 <jasminewu101@gmail.com>
2023-10-27 10:13:16 -0400

    piwaejtv wmtpoihw4

--- dom.js
+++ dom.js
@@ -3,41 +3,45 @@ const DOMselectors = {
     artistname: document.querySelector("name"),
     image: document.querySelector("image"),
     card: document.querySelector("card"),
-    button: querySelectorAll("button")
+    button: querySelectorAll("button"),
+    card: document.querySelector("card"),
 };
-console.log(DOMselectors);
-
-// let me cook pls
-DOMselectors.addEventListener('click', function(ok){
-     DOMselectors..forEach(element => {
-        
-     });
-     }
-});
-
-const addForm = document.forms['add-album'];
-
-addForm.addEventListener('submit', function(ok){
+DOMselectors.addForm.addEventListener('submit', function(ok){
     ok.preventDefault();
-    document.querySelector('input[type="text"]').value;
-    addForm.card.insertAdjacentHTML(afterend, 
+    console.log(DOMselectors.image.value, DOMselectors.artistname.albumname, DOMselectors.image.value);
+    function cards (){
+    cards.preventDefault();
+    DOMselectors.card.insertAdjacentHTML(afterend, 
     `<div class="gallery">
     <section class="card">
     <img class=card-img src="${DOMselectors.image.value} alt="album photo">
     <h2 class="displayalbumname">${DOMselectors.albumname.value}</h2>
-    <h2 class="displayartist">${"name"}</h2>
+    <h2 class="displayartist">${DOMselectors.artistname.value}</h2>
     <button class="delete">delete</button>
     </section>
-</div>`);
-}); 
-
-    albumName.textContent = value;
-
+    </div>`)}
+cards()
+remove()
+clearfields()
+});
     
-/*DOMselectors.button.addEventListener("click", function (){
-    box(DOMselectors.box, DOMselectors.text);
-}); 
+
+function clearfields()
+    DOMselectors.albumname.value = ""
+    DOMselectors.artistname.value = ""
+    DOMselectors.picture.value = ""
+    
+
 
 //const album = make album
 //addcard(album)
 //clear fields addremove button*/
+
+function remove(){
+    let btns = document.querySelectorAll("button");
+    btns.forEach((button)=>
+     button.addEventListener('click', function(event){
+     console.log(event.target.parentELement);
+    })
+  );
+}

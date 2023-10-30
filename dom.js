50542d286651b35e036a7029c0f033af080fa06a
jasminew13 <jasminewu101@gmail.com>
2023-10-30 10:13:35 -0400

    sos

--- dom.js
+++ dom.js
@@ -1,41 +1,35 @@
 const DOMselectors = {
-    albumname: document.querySelector("album"),
-    artistname: document.querySelector("name"),
-    image: document.querySelector("image"),
-    card: document.querySelector("card"),
-    button: querySelectorAll("button"),
-    card: document.querySelector("card"),
+    albumname: document.getElementById(".album"),
+    artistname: document.getElementById(".name"),
+    image: document.getElementById(".image"),
+    card: document.getElementById(".card"),
+    submit: document.getElementById(".submit"),
+    button: document.getElementById(".button"),
 };
-DOMselectors.addForm.addEventListener('submit', function(ok){
+DOMselectors.submit.addEventListener('submit', function (ok) {
     ok.preventDefault();
-    console.log(DOMselectors.image.value, DOMselectors.artistname.albumname, DOMselectors.image.value);
+    console.log(DOMselectors.image.value, DOMselectors.artistname.albumname, DOMselectors.image.value)
+    
     function cards (){
-    cards.preventDefault();
-    DOMselectors.card.insertAdjacentHTML(afterend, 
-    `<div class="gallery">
-    <section class="card">
-    <img class=card-img src="${DOMselectors.image.value} alt="album photo">
+    DOMselectors.gallery.insertAdjacentHTML("afterend", 
+    `<section class="card">
+    <img src=${DOMselectors.image.value} class=card-img>
     <h2 class="displayalbumname">${DOMselectors.albumname.value}</h2>
     <h2 class="displayartist">${DOMselectors.artistname.value}</h2>
     <button class="delete">delete</button>
-    </section>
-    </div>`)}
+    </section>`
+    )}
 cards()
-remove()
 clearfields()
+remove()
 });
     
-
-function clearfields()
+function clearfields(){
     DOMselectors.albumname.value = ""
     DOMselectors.artistname.value = ""
     DOMselectors.picture.value = ""
-    
+}
 
-
-//const album = make album
-//addcard(album)
-//clear fields addremove button*/
 
 function remove(){
     let btns = document.querySelectorAll("button");
